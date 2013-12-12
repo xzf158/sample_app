de8557f1a242b91513f049ee1e4674d4dfde283f
xuzhanfu <terry@fpi.cc>
2013-12-12 13:22:49 +0800

    update rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
   # let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
+
   subject { page }
   describe "Home page" do
     before { visit root_path }
@@ -21,13 +23,28 @@ describe "Static pages" do
     before { visit about_path }
 
     it {should have_content('About')}
-    it {should have_title(full_title('About'))}
+    it {should have_title(full_title('About Us'))}
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it {should have_content('Contact')}
+    it {should have_selector('h1', text: 'Contact')}
     it {should have_title(full_title('Contact'))}
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title(''))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
   end
 end
